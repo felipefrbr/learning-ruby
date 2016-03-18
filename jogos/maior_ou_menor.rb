2dc9be68a2125f37c9d0870af7a4827d9ec34bed
felipefrbr <felipefrbr@gmail.com>
2016-03-18 22:03:14 +0000

    Atribuição de variável

--- jogos/maior_ou_menor.rb
+++ jogos/maior_ou_menor.rb
@@ -9,3 +9,16 @@ puts
 puts
 puts
 puts "Começaremos o jogo para você, " + nome
+
+puts "Escolhendo um número secreto entre 0 e 200..."
+numero_secreto = 175
+puts "Escolhido...  que tal advinhar hoje nosso número secreto?"
+
+puts
+puts
+puts
+puts
+puts "Tentativa 1"
+puts "Entre com o número"
+chute = gets
+puts "Será que acertou? Você chutou " + chute
