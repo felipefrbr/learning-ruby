49b0a2eb8b6e3d574522c6d5ceea1276e18a756c
Felipe Ribeiro <felipefrbr@gmail.com>
2016-03-25 00:10:32 -0300

    limite de tentativas

--- jogos/maior_ou_menor.rb
+++ jogos/maior_ou_menor.rb
@@ -8,60 +8,26 @@ puts "Escolhendo um número secreto entre 0 e 200..."
 numero_secreto = 175
 puts "Escolhido...  que tal advinhar hoje nosso número secreto?"
 
-puts "Tentativa 1"
-puts "Entre com o número"
-chute = gets
-puts "Será que acertou? Você chutou " + chute
+limite_de_tentativas = 5
 
-acertou = chute.to_i == numero_secreto
-
-if acertou
-	puts "Acertou!"
-else
-	maior = numero_secreto > chute.to_i
-	if maior
-		puts "O número secreto é maior"
-	else
-		puts "O número secreto é menor"
-	end
-
-end
-
-
-puts "Tentativa 2"
-puts "Entre com o número"
-chute = gets
-puts "Será que acertou? Você chutou " + chute
-
-acertou = chute.to_i == numero_secreto
-
-if acertou
-    puts "Acertou!"
-    else
-    maior = numero_secreto > chute.to_i
-    if maior
-        puts "O número secreto é maior"
-        else
-        puts "O número secreto é menor"
-    end
+for tentativa in 1..limite_de_tentativas
+    puts "Tentativa " + tentativa.to_s + " de " + limite_de_tentativas.to_s
+    puts "Entre com o número"
+    chute = gets
+    puts "Será que acertou? Você chutou " + chute
     
-end
-
-puts "Tentativa 3"
-puts "Entre com o número"
-chute = gets
-puts "Será que acertou? Você chutou " + chute
-
-acertou = chute.to_i == numero_secreto
-
-if acertou
-    puts "Acertou!"
+    acertou = chute.to_i == numero_secreto
+    
+    if acertou
+        puts "Acertou!"
     else
-    maior = numero_secreto > chute.to_i
-    if maior
-        puts "O número secreto é maior"
+        maior = numero_secreto > chute.to_i
+        if maior
+            puts "O número secreto é maior"
         else
-        puts "O número secreto é menor"
+            puts "O número secreto é menor"
+        end
+        
     end
     
 end
