20273b1e63444cb535ef38a635cbca1146017c4c
Felipe Ribeiro <felipefrbr@gmail.com>
2016-03-25 00:00:48 -0300

    laço for

--- jogos/testes.rb
+++ jogos/testes.rb
@@ -1,15 +1,3 @@
-puts 175 == 175
-puts 175 == 174
-puts "175" == 175
-puts "175" == "175"
-puts "175".to_i == 175
-
-puts
-
-puts 175 != 174
-puts 175 != 175
-
-puts 175 > 174
-puts 175 >= 175
-puts 175 < 176
-puts 175 <= 175
+for dedos in 1..3
+    puts 'contando ' + dedos.to_s
+end
