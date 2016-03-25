97d35659867b035b0c0cb495a81f813bb4a7ab4c
Felipe Ribeiro <felipefrbr@gmail.com>
2016-03-24 23:31:42 -0300

    if else

--- jogos/maior_ou_menor.rb
+++ jogos/maior_ou_menor.rb
@@ -2,24 +2,19 @@ puts "Bem vindo ao jogo da adivinhação"
 puts "Qual é o seu nome?"
 nome = gets
 
-puts
-puts
-puts
-puts
-puts
-puts
 puts "Começaremos o jogo para você, " + nome
 
 puts "Escolhendo um número secreto entre 0 e 200..."
 numero_secreto = 175
 puts "Escolhido...  que tal advinhar hoje nosso número secreto?"
 
-puts
-puts
-puts
-puts
 puts "Tentativa 1"
 puts "Entre com o número"
 chute = gets
 puts "Será que acertou? Você chutou " + chute
-puts chute.to_i == numero_secreto
+
+if chute.to_i == numero_secreto
+	puts "Acertou!"
+else
+	puts "Errou!"
+end
