de7e245eb10b51c094dcc11ab815baa5c274ac91
Felipe Ribeiro <felipe.ribeiro@m4u.com.br>
2016-04-13 17:36:01 -0300

    verificando se a letra existe

--- jogos/forca.rb
+++ jogos/forca.rb
@@ -40,6 +40,32 @@ def joga(nome)
     while erros < 5
         chute = pede_um_chute(chutes, erros)
         chutes << chute
+
+        chutou_uma_letra = chute.size == 1
+        if chutou_uma_letra
+            letra_procurada = chute[0]
+            total_encontrado = palavra_secreta.count(letra_procurada)
+
+            if total_encontrado == 0
+                puts "Letra não encontrada."
+                erros += 1
+            else
+                puts "Letra encontrada #{total_encontrado} vezes."
+            end
+
+        else
+            acertou = chute == palavra_secreta
+            if(acertou)
+                puts "Parabéns! Acertou..."
+                pontos_ate_agora += 100
+                break
+            else
+                puts "Que pena... errou!"
+                pontos_ate_agora -= 30
+                erros += 1
+            end
+        end
+
     end
 
     puts "Você ganhou #{pontos_ate_agora} pontos."
